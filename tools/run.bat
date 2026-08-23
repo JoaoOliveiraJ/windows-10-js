f70b53e88ea391fc231095c0db18f82a53f28e91
JoaoOliveiraJ <novojoaojm@gmail.com>
2026-08-23 19:20:53 -0300

    run.bat: normaliza o caminho do serial.log (espaco + .. quebrava o QEMU)

--- tools/run.bat
+++ tools/run.bat
@@ -1,10 +1,12 @@
 @echo off
 rem tools\run.bat - boot interativo do jsOS (janela VGA + aceleracao WHPX)
 rem Boot BIOS (SeaBIOS) a partir de build\os.img; serial vai p/ build\serial.log
+for %%I in ("%~dp0..\build") do set "BUILDDIR=%%~fI"
+if not exist "%BUILDDIR%\serial.log" type nul > "%BUILDDIR%\serial.log"
 "C:\Program Files\qemu\qemu-system-x86_64.exe" ^
   -m 4096 -smp 4 -accel whpx -machine pc,kernel-irqchip=off ^
-  -drive format=raw,file="%~dp0..\build\os.img" ^
-  -drive format=raw,if=ide,index=1,media=disk,file="%~dp0..\build\ntfs.img" ^
+  -drive format=raw,file="%BUILDDIR%\os.img" ^
+  -drive format=raw,if=ide,index=1,media=disk,file="%BUILDDIR%\ntfs.img" ^
   -display sdl ^
-  -serial file:"%~dp0..\build\serial.log" ^
+  -serial file:"%BUILDDIR%\serial.log" ^
   -no-reboot
