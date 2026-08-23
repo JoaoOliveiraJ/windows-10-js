8ff0404541d9f13fc287fe35cca6c1aabef3c3ec
JoaoOliveiraJ <novojoaojm@gmail.com>
2026-08-23 19:18:21 -0300

    run.bat: display sdl (captura teclado p/ o shell interativo)

--- tools/run.bat
+++ tools/run.bat
@@ -5,5 +5,6 @@ rem Boot BIOS (SeaBIOS) a partir de build\os.img; serial vai p/ build\serial.log
   -m 4096 -smp 4 -accel whpx -machine pc,kernel-irqchip=off ^
   -drive format=raw,file="%~dp0..\build\os.img" ^
   -drive format=raw,if=ide,index=1,media=disk,file="%~dp0..\build\ntfs.img" ^
+  -display sdl ^
   -serial file:"%~dp0..\build\serial.log" ^
   -no-reboot
